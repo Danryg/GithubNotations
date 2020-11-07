fcfadf7288e6999592917bae52294f4a0dfb3c12
DanielRygaard <daniel.rygaard@live.se>
2020-11-07 11:35:04 +0100

    Adding the request example

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const app = express();
 const port = process.env.PORT = 3000;
 
 
+var options = {
+    url: 'https://api.github.com/users/lillelink',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+request(options, function (error, response, body) {
+
+    var info = JSON.parse(body);
+  console.log('body:', info); // Print the HTML for the Google homepage.
+  
+});
 
 
-app.listen(port, ()=> console.log('Listening to port: ${port}.......'));
+
+
+
+
+
+app.listen(port, () => console.log(`Listening to port: ${port}.....`));
